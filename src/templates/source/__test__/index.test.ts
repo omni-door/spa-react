29646f18d1a248ce88438a2c75af264cfe9263d8
bobby.li <bobby.lhan@gmail.com>
2020-03-16 18:58:49 +0800

    unit-test fixed

--- src/templates/source/__test__/index.test.ts
+++ src/templates/source/__test__/index.test.ts
@@ -2,7 +2,6 @@ import { describe, it } from 'mocha';
 import { expect } from 'chai';
 import source_declaration from '../declaration';
 import source_html from '../html';
-import source_index from '../index';
 import source_index_react from '../index_react';
 
 describe('source_declaration template test', function () {
@@ -14,12 +13,6 @@ describe('source_declaration template test', function () {
 describe('source_html template test', function () {
   it('type checking', function () {
     expect(source_html).to.be.a('function');
-  });
-});
-
-describe('source_index template test', function () {
-  it('type checking', function () {
-    expect(source_index).to.be.a('function');
   });
 });
 
